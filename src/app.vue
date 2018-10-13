4b5b087f642c04b42089385e417def2effae141f
tongdongsheng <tongdongsheng@kuaishou.com>
2018-10-14 00:57:54 +0800

    vue项目基本环境配置

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>dd {{text}}</div>
+</template>
+
+<script type="text/javascript">
+    export default {
+    	data() {
+            return {
+                text : "hello"
+            }
+        }
+    }
+</script>
+
+<style type="text/css">
+
+
+</style>
